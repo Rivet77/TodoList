45a008617a5cbd45e2555d00dd1073ccba1dfd9e
Rivet77 <33362056+Rivet77@users.noreply.github.com>
2019-10-24 19:17:39 -0400

    v1.0.0
    straight from the crash course

---
+++ precache-manifest.fd4f986ee4e51e538ea61ee4a29f757f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec04b3c7f04d15cbfb262d3e71ad32a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf0aa0394ca5ffbe14de",
+    "url": "/static/css/main.95ea5ae8.chunk.css"
+  },
+  {
+    "revision": "b58f73f242d8052d10ab",
+    "url": "/static/js/2.27573666.chunk.js"
+  },
+  {
+    "revision": "bf0aa0394ca5ffbe14de",
+    "url": "/static/js/main.19a480ec.chunk.js"
+  },
+  {
+    "revision": "fca03bb5f1614bf42f69",
+    "url": "/static/js/runtime-main.f6b1f150.js"
+  }
+]);
